86dc3fea9058a68aae01decf8a79868ad9946a4f
kimhyunho <kim4989b@naver.com>
2024-04-25 02:03:19 +0900

    1.1

---
+++ js/common.js
@@ -0,0 +1,47 @@
+console.log("init=============");
+
+
+
+ function oneArray(){
+
+    //1차원배열 
+    //요소(element)1,2,3,4,5 
+    var array=[1,2,3,4,5];
+    // array.length 5개 
+
+    for(var i=0;i<array.length;i++){
+        console.log(array[i]);
+
+    }
+
+ }
+
+console.log(oneArray());
+ 
+
+
+
+
+
+
+
+
+
+function twoArray(){
+
+    // 2차원배열 1  ~ 10 입력하고 출력하시오 
+     var array=[];
+
+
+    // for(var i=0;i<2;i++){
+            
+    //     for(x=0;x<5;x++){
+    //         array[i][x]=++init
+
+    //     }
+
+
+    // }
+
+
+}
